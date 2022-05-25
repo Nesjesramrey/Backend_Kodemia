f8076d73d9f509427ce7f4a7ab621394f45ce83d
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-05-24 20:33:34 -0500

    add solution create, read and delete files

---
+++ sesion_04/js/sesion04.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+//console.log(fs)
+
+
+// fs.writefile si no existe el archivo lo crea y si existe lo sobrescribe
+// fs.writeFile("hola.txt", "Mi primer archivo", (err) => {
+//   if (err) {
+//     console.error("Algo salio mal:", err);
+//   } else {
+//     console.log("Se creo el archivo! :)");
+//   }
+// });
+
+function createFile(name, content) {
+  fs.writeFile(name, content, (err) => {
+    if (err) {
+      console.error("Algo salio mal:", err);
+    } else {
+      console.log("Se creo el archivo! :)");
+    }
+  });
+}
+
+createFile("hola.txt", "Mi primer archivo")
+
+//** ReadFile */
+function readFile(name, encoding) {
+    fs.readFile(name, encoding, (err) => {
+      if (err) {
+        console.error("No lo pude leer:", err);
+      } else {
+        console.log("Se leyo el archivo! : ",);
+      }
+    });
+  }
+  readFile("hola.txt", "utf8")
+
+
+  //** DeleteFile */
+function deleteFile(name) {
+    fs.unlink(name, (err) => {
+      if (err) {
+        console.error("No se encontro:", err);
+      } else {
+        console.log('Archivo', name, 'eliminado');
+      }
+    });
+  }
+  deleteFile("hola.txt")
+
